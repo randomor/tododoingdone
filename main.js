ce9831abdc056c7bcd528e56f232909700384461
Exploria <randomor@gmail.com>
2012-02-20 13:10:44 -0600

    added backbone for todo-list

--- main.js
+++ main.js
@@ -1,31 +1,42 @@
 jQuery(function(){
         $("li").draggable();
         
-    //     window.ThreadListView = Backbone.View.extend({
-    //     tagName: "li"
-    //     
-    //     , className: ""
-    //     
-    //     , events: {
-    //         "click": "open"
-    //     }
-    // 
-    //     , template: _.template($('template').html())
-    // 
-    //     , render: function(){
-    //         $(this.el).html(this.template(this.model))
-    //         ; return this;
-    //     }
-    // 
-    //     , open: function(){
-    // 
-    //     }
-    // })
-    // 
-    // 
-    // ; $.getJSON('main.json', function(r){
-    //         new MailApp({model: r});
-    //     }); 
+        window.TodoView = Backbone.View.extend({
+        tagName: "li"
+        
+        , className: "todo"
+        
+        , events: {
+            "click": "open"
+        }
+    
+        , template: _.template($('#todo-template').html())
+    
+        , render: function(){
+            $(this.el).html(this.template(this.model)).draggable()
+            ; return this;
+        }
+    
+        , open: function(){
+            console.log("just clicked me")
+        }
+    })
+    
+    ; window.TdddApp = Backbone.View.extend({
+        el: $("#tdddApp")
+
+        , initialize: function(){
+            _.each(this.model, function(todo){
+                var todoView = new TodoView({model: todo})
+                ; $("#todo-list").append(todoView.render().el); 
+            })
+            ; return this;
+        }
+    })
+    
+    ; var todoList = [{"title":"Finish paper for CI8133"}, {"title": "Start working on Indelearn"}, {"title": "Start planning for springbreak"}, {"title": "Creating a tddd app"}, {"title": "Get an Intern"}]
+    ;   new TdddApp({model: todoList}); 
+    
 
 
 })

f54e8b1bef5d9e8c9661b608bff1c5cebc6efe32
Exploria <randomor@gmail.com>
2012-02-20 15:22:14 -0600

    added sortable instead of draggable

--- main.js
+++ main.js
@@ -1,5 +1,31 @@
 jQuery(function(){
-        $("li").draggable();
+    $("#tdddApp ul").sortable({
+        connectWith: "#tdddApp ul"
+        , placeholder: "ui-state-highlight"
+    })
+    // ; $("li").draggable({
+    //     containment: "#tadddApp"
+    //     , helper: "clone"
+    //     , stack: "#todo-list"
+    //     , connectToSortable: "#todo-list"
+    // })
+        // ; $( "#tdddApp ul" ).droppable({
+        //     over: function( event, ui ) {
+        //         $( this ).addClass( "ui-state-highlight" );
+        //         console.log("over")
+        //     }
+        //     , out: function(event, ui) {
+        //         $(this).removeClass("ui-state-highlight")
+        //     }
+        //     , drop: function(event, ui){
+        //         if(ui.draggable.parent().attr("id")!=$(this).attr("id")){
+        //             $(this).append(ui.draggable.css({"left": "0px", "top": "0px"}))
+        //         }else{
+        //             console.log("else here")
+        //         };
+        //         $(this).removeClass("ui-state-highlight")
+        //     }
+        // });
         
         window.TodoView = Backbone.View.extend({
         tagName: "li"
@@ -13,11 +39,12 @@ jQuery(function(){
         , template: _.template($('#todo-template').html())
     
         , render: function(){
-            $(this.el).html(this.template(this.model)).draggable()
+            $(this.el).html(this.template(this.model)).css("background-color", this.model.color)
             ; return this;
         }
     
         , open: function(){
+            $(this.el).css("z-index", 10);
             console.log("just clicked me")
         }
     })
@@ -34,9 +61,8 @@ jQuery(function(){
         }
     })
     
-    ; var todoList = [{"title":"Finish paper for CI8133"}, {"title": "Start working on Indelearn"}, {"title": "Start planning for springbreak"}, {"title": "Creating a tddd app"}, {"title": "Get an Intern"}]
-    ;   new TdddApp({model: todoList}); 
+    ; var todoList = [{"title":"Finish paper for CI8133", "color": "#ccf"}, {"title": "Start working on Indelearn", "color": "#ccf"}, {"title": "Start planning for springbreak", "color": "#cfc"}, {"title": "Creating a tddd app", "color": "#cfc"}, {"title": "Get an Intern", "color": "#ccf"}]
+    ; var todoList2 = [{"title":"1", "color": "#ccf"}, {"title": "2", "color": "#ccf"}, {"title": "3", "color": "#cfc"}, {"title": "4", "color": "#cfc"}, {"title": "5", "color": "#ccf"}]    
+    ;   new TdddApp({model: todoList2}); 
     
-
-
 })
